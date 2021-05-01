a616b29b504a6275375644cb590a033535d1a9fc
majerv <viktor.majer1@gmail.com>
2021-05-01 22:31:47 +0200

    Fix REST controller and serialization

--- tradingview-endpoint/src/main/java/com/dave/autotrader/tradingviewendpoint/TradingViewAlertApplication.java
+++ tradingview-endpoint/src/main/java/com/dave/autotrader/tradingviewendpoint/TradingViewAlertApplication.java
@@ -1,14 +1,10 @@
 package com.dave.autotrader.tradingviewendpoint;
 
-import com.dave.autotrader.domain.BuyOrSell;
-import com.dave.autotrader.domain.TradingViewAlert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = TradingViewAlertApplication.class)
 public class TradingViewAlertApplication {
-	private BuyOrSell d;
-	private TradingViewAlert event;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingViewAlertApplication.class, args);
